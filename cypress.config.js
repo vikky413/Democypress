11630509aa98852046be6b5e33acad811bbd9b43
vikram singh <vikramsinghrewa2001@gmail.com>
2022-12-16 15:46:59 +0530

    Add result

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'results/my-test-output.xml',
+      toConsole: true
+    }
   },
 });
